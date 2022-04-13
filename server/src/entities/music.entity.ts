62dbee5464f105a6781053e3a05c037306f949a9
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2022-04-13 20:59:52 +0900

    Change: 음악 엔티티 변경

--- server/src/entities/music.entity.ts
+++ server/src/entities/music.entity.ts
@@ -12,6 +12,36 @@ export enum MusicStatus {
   PRIVATE = 'PRIVATE',
 }
 
+export interface IMusicMetadata {
+  title: string;
+  genre?: string;
+  description?: string;
+  artist?: string;
+  album: string;
+  albumartist?: string;
+  composer?: string;
+  year?: string;
+  lyrics?: string;
+}
+
+export interface IMusicData {
+  title: string;
+  permalink: string;
+  filename: string;
+  link: string;
+  genre?: string;
+  description?: string;
+  tags?: string[];
+  cover?: string;
+  status: MusicStatus;
+  metaData?: IMusicMetadata;
+}
+
+// permalink: string
+//   genre?: string
+//   description?: string
+//   tags?: string[]
+
 @Entity()
 export class Music extends BaseEntity {
   @PrimaryGeneratedColumn()
@@ -21,7 +51,31 @@ export class Music extends BaseEntity {
   title: string;
 
   @Column()
+  permalink: string;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  link: string;
+
+  @Column({ nullable: true })
+  cover: string;
+
+  @Column({ nullable: true })
+  album: string;
+
+  @Column({ nullable: true })
   description: string;
+
+  @Column({ nullable: true })
+  genre: string;
+
+  @Column('text', { array: true, nullable: true })
+  tags: string[];
+
+  @Column('simple-json', { nullable: true })
+  metadata: IMusicMetadata;
 
   @Column()
   status: MusicStatus;
